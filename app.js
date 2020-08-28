e0cab2fe8b6277f8231fa521d2f5d8d302663952
austinolsen <austin.olsen@gmail.com>
2020-08-27 20:21:35 -0700

    added framework for two guesses

--- app.js
+++ app.js
@@ -11,12 +11,34 @@ document.addEventListener('DOMContentLoaded', () => {
       const square = document.createElement('div')
       square.setAttribute('id', i)
       square.setAttribute( 'cardLetter', shuffledArray[i])
-      square.innerHTML = shuffledArray[i]
       grid.appendChild(square)
       squares.push(square)
+
+      //click square
+      square.addEventListener('click', function(e) {
+        click(square)
+      })
     }
   }
   createBoard()
+
+  let firstGuess = []
+  let secondGuess = []
+  let score = 0
+
+  function click(square) {
+    let clickLetter = square.getAttribute('cardLetter')
+    let clickId = square.getAttribute('id')
+    square.innerHTML = clickLetter
+
+    if (firstGuess.length === 0) {
+      firstGuess = [{cardLetter : clickLetter}, {id : clickId}]
+      console.log(firstGuess)
+    } else {
+      secondGuess = [{cardLetter : clickLetter}, {id : clickId}]
+      console.log(secondGuess)
+    }
+  }
 
 
 })
