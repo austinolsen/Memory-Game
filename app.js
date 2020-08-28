de500fa4c9bb64850f5611c285fe9315d8bb8ac6
austinolsen <austin.olsen@gmail.com>
2020-08-27 18:35:39 -0700

    added letters to square and displayed them

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
   let width = 4
   let squares = []
 
+  //create board
   function createBoard() {
+    const cardsArray = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H"]
+    const shuffledArray = cardsArray.sort(() => Math.random() -0.5)
     for(let i = 0; i < width*width; i++) {
       const square = document.createElement('div')
       square.setAttribute('id', i)
+      square.setAttribute( 'cardLetter', shuffledArray[i])
+      square.innerHTML = shuffledArray[i]
       grid.appendChild(square)
       squares.push(square)
     }
