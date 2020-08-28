0bae355277e885091536f8da2fd7e6e624da411e
austinolsen <austin.olsen@gmail.com>
2020-08-27 17:15:24 -0700

    created basic board

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let width = 4
+  let squares = []
+
+  function createBoard() {
+    for(let i = 0; i < width*width; i++) {
+      const square = document.createElement('div')
+      square.setAttribute('id', i)
+      grid.appendChild(square)
+      squares.push(square)
+    }
+  }
+  createBoard()
+
+
+})
